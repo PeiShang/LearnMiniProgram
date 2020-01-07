93849249ba86e0cdff4ef631399e994b5e8d3059
PeiShang <shangpei92@foxmail.com>
2020-01-07 22:26:37 +0800

    数据绑定

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,10 +1,16 @@
-// pages/home/home.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    name:'CoderWhy',
+    age: 18,
+    students:[
+      { id: 100, name:'sp', age:30},
+      { id: 101, name: 'sb', age: 31},
+      { id: 102, name: 'spp', age: 32}
+    ]
 
   },
 
@@ -12,55 +18,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
   }
 })
